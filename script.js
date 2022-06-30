92d24f785130cb2c07c97e295d5ca2a44c03d984
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-06-29 21:28:23 -0300

    finalização

---
+++ script.js
@@ -0,0 +1,21 @@
+function validation(){
+    var nome = document.querySelector("#name").value;
+    var lastName = document.querySelector("#last-name").value;
+    var user = document.querySelector("#email").value.substring(0, document.querySelector("#email").value.indexOf("@"));
+    var domain = document.querySelector("#email").value.substring(document.querySelector("#email").value.indexOf("@") + 1);
+    var password = document.querySelector("#pass").value;
+
+    if(nome.length == 0) {
+        document.querySelector("#val-name").innerHTML = "Nome não pode estar vazio";
+    }
+    if(lastName.length == 0) {
+        document.querySelector("#val-last-name").innerHTML = "Sobrenome não pode estar vazio";
+    } 
+    if((user.length >= 1) && (domain.length >= 3) && (user.search("@") == -1) && (domain.search("@") == -1) && (user.search(" ") == -1) && (domain.search(" ") == -1) && (domain.search(".") != -1) && (domain.indexOf(".") >= 1) && (domain.lastIndexOf(".") < domain.length -1)){
+    } else{
+        document.querySelector("#val-email").innerHTML = "Email não pode estar vazio";
+    }
+    if(password.length == 0){
+        document.querySelector("#val-pass").innerHTML = "Senha não pode estar vazio";
+    }
+}
